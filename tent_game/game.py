b4349d5b80e104b0d647ef377b86a5e745547f46
thanh.truong <thanh.truong@gihot.vn>
2024-04-12 11:57:35 +0700

    tent game 10%

--- tent_game/game.py
+++ tent_game/game.py
@@ -1,8 +1,11 @@
 import pygame, sys, random
 from pygame.locals import *
+from enum import Enum
 
-WINDOWWIDTH = 1920
-WINDOWHEIGHT = 990
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+
+OFFSET = [0,0]
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -16,22 +19,15 @@ WIDTH = 28
 HEIGHT = 28
 # This sets the distance between each cell
 MARGIN = 2
-rownum = 33
-colnum = 64
+rownum = 7
+colnum = 7
 
-grid = []
-for row in range(rownum):
-    # Add an empty array that will hold each cell
-    # in this row
-    grid.append([])
-    for column in range(colnum):
-        grid[row].append(0)  # Append a cell
 
 x_img = pygame.transform.scale(pygame.image.load("tent_game/Assets/tree.jpg"),(28,28))
 o_img = pygame.transform.scale(pygame.image.load("tent_game/Assets/tent.jpg"),(28,28))
 
-BACKGROUND = pygame.image.load('tent_game/Assets/bg.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (WINDOWWIDTH, WINDOWHEIGHT))
+# BACKGROUND = pygame.image.load('tent_game/Assets/bg.png')
+# BACKGROUND = pygame.transform.scale(BACKGROUND, (WINDOWWIDTH, WINDOWHEIGHT))
 pygame.init()
 FPS = 60
 fpsClock = pygame.time.Clock()
@@ -39,21 +35,30 @@ fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.display.set_caption('Tent')
 
-# def main():
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-        
-#         DISPLAYSURF.blit(BACKGROUND, (0, 0))
+class CellValue(Enum):
+    NOTHING = 0
+    TENT = 1
+    TREE = 2
+class Cell:
+    def __init__(self,value : CellValue, position: tuple):
+        self.value = value
+        self.position = position
+class Grid:
+    def __init__(self, rowNum:int, colNum:int,cell):
+        self.row = rowNum
+        self.col = colNum
+        self.grid = []
+        self.cellClass = cell
+    def SetUp(self):
+        for r in range(self.row):
+            self.grid.append([])
+            for c in range(self.col):
+                self.grid[r].append(self.cellClass(CellValue.NOTHING, (0,0)))
+grid = Grid(rownum, colnum, Cell)
+grid.SetUp()
 
-#         pygame.display.update()
-#         fpsClock.tick(FPS)
-# if __name__ == '__main__':
-#     main()
 def main():
-    DISPLAYSURF.blit(BACKGROUND, (0, 0))
+    # DISPLAYSURF.blit(BACKGROUND, (0, 0))
     done = False
     status = None
     while not done:
@@ -61,32 +66,35 @@ def main():
             if event.type == pygame.QUIT:  # If user clicked close
                 done = True  # Flag that we are done so we exit this loop
                 # Set the screen background
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     pos = pygame.mouse.get_pos()
+            #     col = pos[0] 
+            #     row=  pos[1] 
+            #     print(row)
+            #     print(col)
+            #     if grid[row][col] == 0:
+            #         if XO == 'x':
+            #             grid[row][col] = XO
+            #             XO = 'o'
+            #         else:
+            #             grid[row][col] = XO
+            #             XO = 'x'
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                col = pos[0] 
-                row=  pos[1] 
-                print(row)
-                print(col)
-                if grid[row][col] == 0:
-                    if XO == 'x':
-                        grid[row][col] = XO
-                        XO = 'o'
-                    else:
-                        grid[row][col] = XO
-                        XO = 'x'
+                print(pos)
         for row in range(rownum):
             for column in range(colnum):
-                color = WHITE
+                color = BLACK
                 pygame.draw.rect(DISPLAYSURF,
-                                color,
-                                [(MARGIN + WIDTH) * column + MARGIN,
-                                (MARGIN + HEIGHT) * row + MARGIN,
+                                WHITE,
+                                [OFFSET[0]+(MARGIN + WIDTH) * column + MARGIN,
+                                OFFSET[1]+(MARGIN + HEIGHT) * row + MARGIN,
                                 WIDTH,
                                 HEIGHT])
-                if grid[row][column] == 'x': 
-                    DISPLAYSURF.blit(x_img,((WIDTH + MARGIN)*column+2,(HEIGHT + MARGIN)*row+2))
-                if grid[row][column] == 'o':
-                    DISPLAYSURF.blit(o_img,((WIDTH + MARGIN)*column+2,(HEIGHT + MARGIN)*row+2))
+                if grid.grid[row][column].value == CellValue.TENT: 
+                    DISPLAYSURF.blit(x_img,(OFFSET[0]+(WIDTH + MARGIN)*column+2,OFFSET[1]+(HEIGHT + MARGIN)*row+2))
+                if grid.grid[row][column].value == CellValue.TREE:
+                    DISPLAYSURF.blit(o_img,(OFFSET[0]+(WIDTH + MARGIN)*column+2,OFFSET[1]+(HEIGHT + MARGIN)*row+2))
         fpsClock.tick(FPS)
         # Go ahead and update the screen with what we've drawn.
         pygame.display.update()
